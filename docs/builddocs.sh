9bee5994b1839b1a729ef58788634fee86529d3e
RickGelhausen <rick.gelhausen@gmail.com>
2023-05-19 14:54:03 +0200

    updated permission

--- docs/builddocs.sh
+++ docs/builddocs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 
-apt-get update
-apt-get -y install git rsync python3-sphinx
+sudo apt-get update
+sudo apt-get -y install git rsync python3-sphinx
 
 pwd ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
@@ -15,4 +15,40 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 make clean
 make html
 
+#######################
+# Update Github Pages #
+#######################
 
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+docroot=`mktemp -d`
+rsync -av "build/html" "${docroot}/"
+
+pushd "${docroot}"
+
+git init
+git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git checkout -b gh-pages
+
+touch .nojekyll
+
+cat > README.md <<EOF
+# README
+This branch is a cache for the website served from https://RickGelhausen.github.io/blog_posts/.
+
+EOF
+
+git add .
+
+msg="Updating Documentation for commit ${GITHUB_SHA} made on `date -d"@${SOURCE_DATE_EPOCH}" --iso-8601=seconds` from ${GITHUB_REF} by ${GITHUB_ACTOR}"
+git commit -am "${msg}"
+
+# deploy gh-pages
+git push deploy gh-pages --force
+
+# return to main repo sandbox root
+popd
+
+# exit cleanly
+exit 0
