8d29dcb344ff09a298c242e31e2bee831448e162
RickGelhausen <rick.gelhausen@gmail.com>
2023-05-19 15:20:22 +0200

    trying to fix token issue

--- docs/builddocs.sh
+++ docs/builddocs.sh
@@ -28,7 +28,7 @@ rsync -av "build/html" "${docroot}/"
 pushd "${docroot}"
 
 git init
-git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git remote add deploy "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git checkout -b gh-pages
 
 touch .nojekyll
